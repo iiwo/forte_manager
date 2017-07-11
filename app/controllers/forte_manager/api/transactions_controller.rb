15b8b8bedfeb0cea386b77b1d3e5ed7740b979af
Iwo Dziechciarow <iiwo@o2.pl>
2017-07-11 17:22:02 +0200

    hack to allow rails 4 client

--- app/controllers/forte_manager/api/transactions_controller.rb
+++ app/controllers/forte_manager/api/transactions_controller.rb
@@ -1,46 +1,48 @@
 module ForteManager
   module Api
-    class TransactionsController < ActionController::API
-      include Rails::Pagination
-      
-      before_action :authenticate!
+    if ForteManager.server # rails 4 client hack
+      class TransactionsController < ActionController::API
+        include Rails::Pagination
 
-      def index
-        filterrific = initialize_filterrific(
-          Transaction,
-          params[:filterrific],
-          select_options: {
-            with_status: Transaction.options_for_status,
-            with_action: Transaction.options_for_action,
-            with_response_code: Transaction.options_for_response_code,
-            with_type: Transaction.options_for_types
-          }
-        ) or return
-        transactions = filterrific.find
+        before_action :authenticate!
 
-        paginate json: transactions
-      end
+        def index
+          filterrific = initialize_filterrific(
+            Transaction,
+            params[:filterrific],
+            select_options: {
+              with_status: Transaction.options_for_status,
+              with_action: Transaction.options_for_action,
+              with_response_code: Transaction.options_for_response_code,
+              with_type: Transaction.options_for_types
+            }
+          ) or return
+          transactions = filterrific.find
 
-      def statuses
-        render json: Transaction.options_for_status
-      end
-
-      def actions
-        render json: Transaction.options_for_action
-      end
-
-      def response_codes
-        render json: Transaction.options_for_response_code
-      end
-
-      private
-      
-        def authenticate!
-          unless params[:token] == ForteManager.secret_token
-            head(401)
-            false
-          end
+          paginate json: transactions
         end
+
+        def statuses
+          render json: Transaction.options_for_status
+        end
+
+        def actions
+          render json: Transaction.options_for_action
+        end
+
+        def response_codes
+          render json: Transaction.options_for_response_code
+        end
+
+        private
+
+          def authenticate!
+            unless params[:token] == ForteManager.secret_token
+              head(401)
+              false
+            end
+          end
+      end
     end
   end
 end
--- app/models/forte_manager/settlement.rb
+++ app/models/forte_manager/settlement.rb
@@ -1,12 +1,21 @@
 module ForteManager
   class Settlement < ApplicationRecord
-    belongs_to(
-      :parent_transaction,
-      foreign_key: :transaction_id,
-      primary_key: :transaction_id,
-      class_name: 'ForteManager::Transaction',
-      optional: true
-    )
+    if Rails::VERSION::MAJOR > 4
+      belongs_to(
+        :parent_transaction,
+        foreign_key: :transaction_id,
+        primary_key: :transaction_id,
+        class_name: 'ForteManager::Transaction',
+        optional: true
+      )
+    else
+      belongs_to(
+        :parent_transaction,
+        foreign_key: :transaction_id,
+        primary_key: :transaction_id,
+        class_name: 'ForteManager::Transaction'
+      )
+    end
 
     def initialize_with(hash:)
       hash.each do |key, value|
